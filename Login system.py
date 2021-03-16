338dfa371d4aa354de9bf570d7fa79fd0ad36fcc
subash2617 <74961842+subash2617@users.noreply.github.com>
2021-03-16 11:47:51 +0530

    Add files via upload

---
+++ Login system.py
@@ -0,0 +1,57 @@
+from tkinter import *
+
+def register_user():
+    username_info = username.get()
+    password_info = password.get()
+
+    file = open(username_info+".txt","w")
+    file.write(username_info+"\n")
+    file.write(password_info)
+    file.close()
+
+    username_entry.delete(0, END)
+    password_entry.delete(0, END)
+
+    Label(screen1, text="Registration Success", fg="green", font=("calibri",10)).pack()
+
+def register():
+    global screen1
+    screen1 = Toplevel(screen)
+    screen1.title("Register")
+    screen1.geometry("300x250")
+
+    global username
+    global password
+    global username_entry
+    global password_entry
+    username = StringVar()
+    password = StringVar()
+
+    Label(screen1, text="Enter the Details Below").pack()
+    Label(screen1, text="").pack()
+    Label(screen1, text="Username * ").pack()
+    username_entry = Entry(screen1, textvariable=username)
+    username_entry.pack()
+    Label(screen1, text="Password * ").pack()
+    password_entry = Entry(screen1, textvariable=password)
+    password_entry.pack()
+    Label(screen1, text="").pack()
+    Button(screen1, text="Register", width=10, height=1, command=register_user).pack()
+
+def login():
+    Label(screen, text="Login Session Started", fg="green", font=("calibri", 10)).pack()
+
+def main_screen():
+    global screen
+    screen = Tk()
+    screen.geometry("300x250")
+    screen.title("Login Info")
+    Label(text= "XYZ", bg="#cecece", width=300, height=2, font=("calibri",13)).pack()
+    Label(text="").pack()
+    Button(text="Login", width=30, height=2, command=login).pack()
+    Label(text="").pack()
+    Button(text="Register", width=30, height=2, command=register).pack()
+
+    screen.mainloop()
+
+main_screen()
